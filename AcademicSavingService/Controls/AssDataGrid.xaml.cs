f5e895fae9e604866b674265a4d502412d9c6509
Nguyen Pham <19521918@gm.uit.edu.vn>
2021-06-11 23:05:57 +0700

    Add search bar

---
+++ AcademicSavingService/Controls/AssDataGrid.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using PropertyChanged;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AcademicSavingService.Controls
+{
+	/// <summary>
+	/// Interaction logic for AssDataGrid.xaml
+	/// </summary>
+	[AddINotifyPropertyChangedInterface]
+	public partial class AssDataGrid : UserControl, INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
+
+		public static readonly DependencyProperty EnableFullTextSearchProperty =
+			DependencyProperty.Register("EnableFullTextSearch", typeof(bool), typeof(AssDataGrid));
+		public bool EnableFullTextSearch
+		{
+			get { return (bool)GetValue(EnableFullTextSearchProperty); }
+			set { SetValue(EnableFullTextSearchProperty, value); }
+		}
+
+		public static readonly DependencyProperty ItemsSourceProperty =
+			DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(AssDataGrid));
+		public IEnumerable ItemsSource 
+		{ 
+			get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+			set { SetValue(ItemsSourceProperty, value); }
+		}
+
+		public int ItemsSize
+		{
+			get { return dataGrid.Items.Count; }
+		}
+
+		public static readonly DependencyProperty SearchTextProperty =
+			DependencyProperty.Register("SearchText", typeof(string), typeof(AssDataGrid));
+		public string SearchText
+		{
+			get { return (string)GetValue(SearchTextProperty); }
+			set { SetValue(SearchTextProperty, value); }
+		}
+
+		public ObservableCollection<DataGridColumn> Columns
+		{ get { return dataGrid.Columns; } }
+
+		protected CancellationTokenSource cancelCulture;
+		protected bool isSearching = false;
+		protected Task currentSearch;
+
+		public AssDataGrid()
+		{
+			InitializeComponent();
+			content.DataContext = this;
+		}
+
+		protected async void txtFullTextSearch_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (isSearching && cancelCulture != null)
+			{
+				cancelCulture.Cancel();
+				await currentSearch;
+				cancelCulture.Dispose();
+			}
+			cancelCulture = new CancellationTokenSource();
+			currentSearch = Search(SearchText, ItemsSize, cancelCulture);
+		}
+
+		protected Task Search(string searchText, int size, CancellationTokenSource token)
+		{
+			isSearching = true;
+			Task task = null;
+
+			task = Task.Run(() =>
+			{
+				for (int i = 0; i < size; i++)
+				{
+					dataGrid.Dispatcher.Invoke(() =>
+					{
+						DataGridRow row = (DataGridRow)dataGrid.ItemContainerGenerator.ContainerFromIndex(i);
+						bool show = false;
+						foreach (var item in dataGrid.Items[i].GetType().GetProperties())
+						{
+							if (System.Convert.ToString(item.GetValue(dataGrid.Items[i])).ToLower().Contains(searchText))
+							{
+								show = true;
+								break;
+							}
+						}
+						if (show)
+							row.Visibility = Visibility.Visible;
+						else
+							row.Visibility = Visibility.Collapsed;
+					});
+
+					if (token.IsCancellationRequested)
+						throw new TaskCanceledException(task);
+				}
+			});
+
+			return task;
+		}
+	}
+}
---
+++ AcademicSavingService/Converters/SearchToVisability.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AcademicSavingService.Converters
+{
+	class SearchToVisability : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)values[0] == "") return Visibility.Visible;
+
+            var searchText = ((string)values[0]).ToLower();
+            var row = values[1];
+            foreach (var p in row.GetType().GetProperties())
+            {
+                if (System.Convert.ToString(p.GetValue(row)).ToLower().Contains(searchText))
+                    return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
