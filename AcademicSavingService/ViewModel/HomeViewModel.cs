ef6c9b67e99e92f9bb4d1bf6e8396c7557331d44
Nguyen Pham <19521918@gm.uit.edu.vn>
2021-06-18 00:34:26 +0700

    here your papafranku

--- AcademicSavingService/ViewModel/HomeViewModel.cs
+++ AcademicSavingService/ViewModel/HomeViewModel.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AcademicSavingService.ViewModel
 {
 	class HomeViewModel : MenuItemViewModel
 	{
+		public DateTime NOW { get; set; } = DateTime.Now;
+		public double PAPAFRANKU { get; set; } = 0;
+		protected CancellationTokenSource token;
+
 		public HomeViewModel(MainViewModel mainViewModel) : base(mainViewModel)
 		{
+			token = new CancellationTokenSource();
+			Task.Run(() =>
+			{
+				while (!token.IsCancellationRequested)
+				{
+					if (NOW != DateTime.Now)
+						NOW = DateTime.Now;
+					if (NOW.Second == 59)
+					{
+						PAPAFRANKU = 1;
+						Task.Delay(1000);
+						PAPAFRANKU = 0;
+					}
+					Task.Delay(1000);
+				}
+			});
+		}
+
+		~HomeViewModel()
+		{
+			token.Cancel();
 		}
 	}
 }
