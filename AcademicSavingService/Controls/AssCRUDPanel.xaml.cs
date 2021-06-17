37cc6bb9718467ba701a5c0fa1a0d6bb7e756349
Nguyen Pham <19521918@gm.uit.edu.vn>
2021-06-17 12:17:30 +0700

    cum

--- AcademicSavingService/Controls/AssCRUDPanel.xaml.cs
+++ AcademicSavingService/Controls/AssCRUDPanel.xaml.cs
@@ -22,8 +22,82 @@ namespace AcademicSavingService.Controls
 			set { middleChild.Content = value; }
 		}
 
+		public static readonly DependencyProperty LeftMarginProperty = DependencyProperty.Register(
+			"LeftMargin",
+			typeof(GridLength),
+			typeof(AssCRUDPanel)
+		);
+		public GridLength LeftMargin
+		{
+			get { return (GridLength)GetValue(LeftMarginProperty); }
+			set { SetValue(LeftMarginProperty, value); }
+		}
+
+		public static readonly DependencyProperty RightMarginProperty = DependencyProperty.Register(
+			"RightMargin",
+			typeof(GridLength),
+			typeof(AssCRUDPanel)
+		);
+		public GridLength RightMargin
+		{
+			get { return (GridLength)GetValue(RightMarginProperty); }
+			set { SetValue(RightMarginProperty, value); }
+		}
+
+		public static readonly DependencyProperty MaxLeftMarginProperty = DependencyProperty.Register(
+			"MaxLeftMargin",
+			typeof(double),
+			typeof(AssCRUDPanel)
+		);
+		public double MaxLeftMargin
+		{
+			get { return (double)GetValue(MaxLeftMarginProperty); }
+			set { SetValue(MaxLeftMarginProperty, value); }
+		}
+
+		public static readonly DependencyProperty MaxRightMarginProperty = DependencyProperty.Register(
+			"MaxRightMargin",
+			typeof(double),
+			typeof(AssCRUDPanel)
+		);
+		public double MaxRightMargin
+		{
+			get { return (double)GetValue(MaxRightMarginProperty); }
+			set { SetValue(MaxRightMarginProperty, value); }
+		}
+
+		public static readonly DependencyProperty MinLeftMarginProperty = DependencyProperty.Register(
+			"MinLeftMargin",
+			typeof(double),
+			typeof(AssCRUDPanel)
+		);
+		public double MinLeftMargin
+		{
+			get { return (double)GetValue(MinLeftMarginProperty); }
+			set { SetValue(MinLeftMarginProperty, value); }
+		}
+
+		public static readonly DependencyProperty MinRightMarginProperty = DependencyProperty.Register(
+			"MinRightMargin",
+			typeof(double),
+			typeof(AssCRUDPanel)
+		);
+		public double MinRightMargin
+		{
+			get { return (double)GetValue(MinRightMarginProperty); }
+			set { SetValue(MinRightMarginProperty, value); }
+		}
+
 		public AssCRUDPanel()
 		{
+			MaxRightMargin = 200;
+			MinRightMargin = 80;
+
+			MaxLeftMargin = 200;
+			MinLeftMargin = 80;
+
+			RightMargin = new GridLength(40, GridUnitType.Star);
+			LeftMargin = new GridLength(40, GridUnitType.Star);
 			InitializeComponent();
 		}
 	}
